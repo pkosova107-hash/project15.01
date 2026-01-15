d8669ed7aa0c3d099f8200b8c849afad8a784b39
Polina Kosova <pkosova107@gmail.com>
2026-01-15 15:29:44 +0300

    улучшение логики main

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -4,11 +4,12 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("1111");
+        System.out.println("----");
     }
 
     public static void doSomething() {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
     }
 }
